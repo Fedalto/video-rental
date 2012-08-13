9a92f91185f65cdd09fe0d6dff04f2681796ab23
Leonardo S. P. Fedalto <lfedalto@gmail.com>
2012-08-13 11:38:29 +0530

    refactor frequent rental points.

--- src/com/twu/refactor/Rental.java
+++ src/com/twu/refactor/Rental.java
@@ -42,11 +42,9 @@ public class Rental {
 
 
     public int calculateFrequentRenterPoints() {
-        int frequentRenterPoints = 1;
-        // add bonus for a two day new release rental
         if ((getMovie().getPriceCode() == Movie.NEW_RELEASE)
                 && getDaysRented() > 1)
-            frequentRenterPoints++;
-        return frequentRenterPoints;
+            return  2;
+        return 1;
     }
 }
--- test/com/twu/refactor/RentalTest.java
+++ test/com/twu/refactor/RentalTest.java
@@ -2,22 +2,21 @@ package com.twu.refactor;
 
 import junit.framework.TestCase;
 
-/**
- * Created with IntelliJ IDEA.
- * User: fedalto
- * Date: 8/13/12
- * Time: 10:44 AM
- * To change this template use File | Settings | File Templates.
- */
 public class RentalTest extends TestCase {
     public void testCalculateAmount() throws Exception {
         Movie python = new Movie("Monty Python and the Holy Grail", Movie.REGULAR);
         Rental rental=new Rental(python,3);
         assertEquals(rental.calculateAmount(),3.5);
     }
-    public void testCalculateFrequentRenterPoints(){
-        Movie python = new Movie("Monty Python and the Holy Grail", Movie.NEW_RELEASE);
-        Rental rental=new Rental(python,3);
+    public void testCalculateFrequentRenterPointsNewRelease(){
+        Movie trek = new Movie("Star Trek 13.2", Movie.NEW_RELEASE);
+        Rental rental=new Rental(trek, 3);
         assertEquals(rental.calculateFrequentRenterPoints(),2);
+    }
+
+    public void testCalculateFrequentRenterPoints(){
+        Movie python = new Movie("Monty Python and the Holy Grail", Movie.REGULAR);
+        Rental rental=new Rental(python, 3);
+        assertEquals(rental.calculateFrequentRenterPoints(), 1);
     }
 }
